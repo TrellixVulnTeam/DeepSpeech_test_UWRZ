19828fceb836cd30aeabee12fa9d9134b1991129
Charmed <rabeb.bayoudhi@gmail.com>
2021-04-22 14:59:35 +0100

    change setup.py

--- setup.py
+++ setup.py
@@ -1,131 +1,107 @@
+#! /usr/bin/env python
+
+from setuptools import setup, Extension
+from distutils.command.build import build
+
 import os
-import platform
+import subprocess
 import sys
-from pathlib import Path
-
-from pkg_resources import parse_version
-from setuptools import find_packages, setup
-
-
-def get_tc_decoder_pkg_url(version, artifacts_root):
-    assert artifacts_root
-
-    ds_version = parse_version(version)
-    branch = "v{}".format(version)
-
-    plat = platform.system().lower()
-    arch = platform.machine().lower()
-
-    if plat == 'linux' and arch == 'x86_64':
-        plat = 'manylinux1'
-
-    if plat == 'darwin':
-        plat = 'macosx_10_10'
-
-    if plat == 'windows':
-        plat = 'win'
-
-    # ABI does not contain m / mu anymore after Python 3.8
-    if sys.version_info.major == 3 and sys.version_info.minor >= 8:
-        m_or_mu = ''
-    else:
-        is_ucs2 = sys.maxunicode < 0x10ffff
-        m_or_mu = 'mu' if is_ucs2 else 'm'
-
-    pyver = ''.join(str(i) for i in sys.version_info[0:2])
-
-    return 'ds_ctcdecoder @ {artifacts_root}/ds_ctcdecoder-{ds_version}-cp{pyver}-cp{pyver}{m_or_mu}-{platform}_{arch}.whl'.format(
-        artifacts_root=artifacts_root,
-        ds_version=ds_version,
-        pyver=pyver,
-        m_or_mu=m_or_mu,
-        platform=plat,
-        arch=arch,
-    )
-
 
 def main():
-    version_file = Path(__file__).parent / 'VERSION'
-    with open(str(version_file)) as fin:
-        version = fin.read().strip()
+    try:
+        import numpy
+        try:
+            numpy_include = numpy.get_include()
+        except AttributeError:
+            numpy_include = numpy.get_numpy_include()
+    except ImportError:
+        numpy_include = ''
+        assert 'NUMPY_INCLUDE' in os.environ
 
-    install_requires_base = [
-        'numpy',
-        'progressbar2',
-        'six',
-        'pyxdg',
-        'attrdict',
-        'absl-py',
-        'semver',
-        'opuslib == 2.0.0',
-        'optuna',
-        'sox',
-        'bs4',
-        'pandas',
-        'requests',
-        'numba == 0.47.0', # ships py3.5 wheel
-        'llvmlite == 0.31.0', # for numba==0.47.0
-        'librosa',
-        'soundfile',
-    ]
+    def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-    decoder_pypi_dep = [
-        'ds_ctcdecoder == {}'.format(version)
-    ]
+    numpy_include = os.getenv('NUMPY_INCLUDE', numpy_include)
+    numpy_min_ver = os.getenv('NUMPY_DEP_VERSION', '')
 
-    tensorflow_pypi_dep = [
-        'tensorflow == 2.4.0'
-    ]
+    project_name = 'deepspeech'
+    if '--project_name' in sys.argv:
+        project_name_idx = sys.argv.index('--project_name')
+        project_name = sys.argv[project_name_idx + 1]
+        sys.argv.remove('--project_name')
+        sys.argv.pop(project_name_idx)
 
-    # Due to pip craziness environment variables are the only consistent way to
-    # get options into this script when doing `pip install`.
-    tc_decoder_artifacts_root = os.environ.get('DECODER_ARTIFACTS_ROOT', '')
-    if tc_decoder_artifacts_root:
-        # We're running inside the TaskCluster environment, override the decoder
-        # package URL with the one we just built.
-        decoder_pkg_url = get_tc_decoder_pkg_url(version, tc_decoder_artifacts_root)
-        install_requires = install_requires_base + [decoder_pkg_url]
-    elif os.environ.get('DS_NODECODER', ''):
-        install_requires = install_requires_base
-    else:
-        install_requires = install_requires_base + decoder_pypi_dep
+    with open('../../training/deepspeech_training/VERSION', 'r') as ver:
+        project_version = ver.read().strip()
 
-    if os.environ.get('DS_NOTENSORFLOW', ''):
-        install_requires = install_requires
-    else:
-        install_requires = install_requires + tensorflow_pypi_dep
+    class BuildExtFirst(build):
+        sub_commands = [('build_ext', build.has_ext_modules),
+                        ('build_py', build.has_pure_modules),
+                        ('build_clib', build.has_c_libraries),
+                        ('build_scripts', build.has_scripts)]
 
-    setup(
-        name='deepspeech',
-        version=version,
-        description=' DeepSpeech inference',
-        url='https://github.com/mozilla/DeepSpeech',
-        author='DeepSpeech authors',
-        license='MPL-2.0',
-        # Classifiers help users find your project by categorizing it.
-        #
-        # For a list of valid classifiers, see https://pypi.org/classifiers/
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Topic :: Multimedia :: Sound/Audio :: Speech',
-            'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-            'Programming Language :: Python :: 3',
-        ],
-        package_dir={'python': 'native_client'},
-        packages=find_packages(where='native_client'),
-        python_requires='>=3.5, <4',
-        install_requires=install_requires,
-        # If there are data files included in your packages that need to be
-        # installed, specify them here.
-        package_data={
-            'deepspeech': [
-                'VERSION',
-                'GRAPH_VERSION',
-            ],
-        },
-    )
+    # Properly pass arguments for linking, setuptools will perform some checks
+    def lib_dirs_split(a):
+        if os.name == 'posix':
+            return a.split('-L')[1:]
+
+        if os.name == 'nt':
+            return []
+
+        raise AssertionError('os.name == java not expected')
+
+    def libs_split(a):
+        if os.name == 'posix':
+            return a.split('-l')[1:]
+
+        if os.name == 'nt':
+            return a.split('.lib')[0:1]
+
+        raise AssertionError('os.name == java not expected')
+
+    ds_ext = Extension(name='deepspeech._impl',
+                       sources=['impl.i'],
+                       include_dirs=[numpy_include, '../'],
+                       library_dirs=list(map(lambda x: x.strip(), lib_dirs_split(os.getenv('MODEL_LDFLAGS', '')))),
+                       libraries=list(map(lambda x: x.strip(), libs_split(os.getenv('MODEL_LIBS', '')))),
+                       swig_opts=['-c++', '-keyword'])
+
+    setup(name=project_name,
+          description='A library for running inference on a DeepSpeech model',
+          long_description=read('README.rst'),
+          long_description_content_type='text/x-rst; charset=UTF-8',
+          author='Mozilla',
+          version=project_version,
+          package_dir={'deepspeech': '.'},
+          cmdclass={'build': BuildExtFirst},
+          license='MPL-2.0',
+          url='https://github.com/mozilla/DeepSpeech',
+          project_urls={
+              'Documentation': 'https://github.com/mozilla/DeepSpeech/tree/v{}#project-deepspeech'.format(project_version),
+              'Tracker': 'https://github.com/mozilla/DeepSpeech/issues',
+              'Repository': 'https://github.com/mozilla/DeepSpeech/tree/v{}'.format(project_version),
+              'Discussions': 'https://discourse.mozilla.org/c/deep-speech',
+          },
+          ext_modules=[ds_ext],
+          py_modules=['deepspeech', 'deepspeech.client', 'deepspeech.impl'],
+          entry_points={'console_scripts':['deepspeech=deepspeech.client:main']},
+          install_requires=['numpy%s' % numpy_min_ver],
+          include_package_data=True,
+          classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Environment :: Console',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3.4',
+              'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3.6',
+              'Topic :: Multimedia :: Sound/Audio :: Speech',
+              'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+              'Topic :: Scientific/Engineering',
+              'Topic :: Utilities',
+          ])
 
 if __name__ == '__main__':
     main()
-
